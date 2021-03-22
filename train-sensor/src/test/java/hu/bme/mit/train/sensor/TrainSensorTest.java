4b7e7f46cb7e3e36c0c28b938ed11d0581932c7f
DaFeee <szalmaskristof@gmail.com>
2021-03-22 17:42:33 +0100

    sonarqube issues fix 2

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,15 +1,11 @@
 package hu.bme.mit.train.sensor;
 
-import hu.bme.mit.train.interfaces.TrainController;
+
 import hu.bme.mit.train.interfaces.TrainSensor;
-import hu.bme.mit.train.interfaces.TrainUser;
-import hu.bme.mit.train.system.TrainSystem;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import hu.bme.mit.train.interfaces.TrainController;
-import hu.bme.mit.train.interfaces.TrainSensor;
-import hu.bme.mit.train.interfaces.TrainUser;
 
 
 import static org.mockito.Mockito.*;
@@ -21,8 +17,6 @@ public class TrainSensorTest {
     @Before
     public void before() {
 
-        TrainSystem system = new TrainSystem();
-        sensor = system.getSensor();
     }
 
     @Test
